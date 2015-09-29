caf5be09ab830a67952561891486b277777180b6
mshirdel <mshirdel@gmail.com>
2015-09-29 10:38:05 +0330

    customizing error messages for forms

--- custom_auth/forms.py
+++ custom_auth/forms.py
@@ -5,18 +5,6 @@ from captcha.fields import CaptchaField
 from .models import UserProfile, ContactUs
 
 
-class ContactUsForm(forms.Form):
-    name = forms.CharField(label='Name', max_length=100, required=False,
-                           widget=forms.TextInput(attrs={'class': 'input-block-level'}))
-    email = forms.EmailField(label='Email', error_messages={'required': 'Email !!!'},
-                             widget=forms.TextInput(attrs={'class': 'input-block-level'}))
-    url = forms.URLField(initial='http://',
-                         widget=forms.TextInput(attrs={'class': 'input-block-level'}))
-    comment = forms.CharField(label='Comment', max_length=1024,
-                              widget=forms.Textarea(attrs={'class': 'input-block-level'}))
-    # cmb = forms.ComboField(fields=[forms.CharField(max_length=20), forms.EmailField()])
-
-
 class SignInForm(forms.Form):
     user_name = forms.CharField(label='نام کاربری', max_length=30,
                                 widget=forms.TextInput(attrs={'class': 'input-block-level'}))
@@ -29,10 +17,18 @@ class SignUpForm(forms.Form):
                                  widget=forms.TextInput(attrs={'class': 'input-block-level'}))
     last_name = forms.CharField(label='نام خانوادگی', max_length=100, required=False,
                                 widget=forms.TextInput(attrs={'class': 'input-block-level'}))
-    user_name = forms.EmailField(label='نام کاربری (ایمیل)', max_length=30,
+    user_name = forms.EmailField(label='نام کاربری (ایمیل)',
+                                 max_length=30,
+                                 error_messages={
+                                     'required': 'لطفا ایمیل خود را وارد کنید',
+                                     'invalid': 'لطفا ایمیل معتبری وارد کنید'
+                                 },
                                  widget=forms.TextInput(attrs={'class': 'input-block-level'}))
-    password1 = forms.CharField(label='کلمه عبور', widget=forms.PasswordInput(attrs={'class': 'input-block-level'}))
+    password1 = forms.CharField(label='کلمه عبور',
+                                error_messages={'required': 'کلمه عبور دلخواه خود را وارد کنید'},
+                                widget=forms.PasswordInput(attrs={'class': 'input-block-level'}))
     password2 = forms.CharField(label='تکرار کلمه عبور',
+                                error_messages={'required': 'کلمه عبور دلخواه خود را وارد کنید'},
                                 widget=forms.PasswordInput(attrs={'class': 'input-block-level'}))
 
 
